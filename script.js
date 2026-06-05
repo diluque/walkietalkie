c770bf6ca418d08db8a0554a00def5feb26bd6ea
diluque <67397718+diluque@users.noreply.github.com>
2026-06-04 21:50:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+/*
+const boton = document.getElementById("ptt");
 
+boton.addEventListener("click", () => {
+    alert("Transmitiendo....");
+});
+*/
+
+const boton = document.getElementById("ptt");
+const aviso = document.getElementById("aviso");
+
+boton.addEventListener("click", () => {
+
+    aviso.style.display = "block";
+
+    setTimeout(() => {
+        aviso.style.display = "none";
+    }, 1000);
+
+});
